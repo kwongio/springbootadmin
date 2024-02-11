b4c3d812bc02c52682c9049fc4540f2dae0e41e0
kwongio <rldh11111@naver.com>
2024-02-11 18:35:45 +0900

    [feat] spring boot admin server 추가

--- src/main/java/org/example/springbootadmin/SpringbootadminApplication.java
+++ src/main/java/org/example/springbootadmin/SpringbootadminApplication.java
@@ -1,8 +1,11 @@
 package org.example.springbootadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableAdminServer
 @SpringBootApplication
 public class SpringbootadminApplication {
 
